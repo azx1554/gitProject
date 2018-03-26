234aecfd1066561b7b014a2c805373f3f6e072df
XiHaoTian <835584505@qq.com>
2018-03-26 17:15:42 +0800

    First commit

---
+++ src/xi/DesignPattern/Moon.java
@@ -0,0 +1,15 @@
+package xi.DesignPattern;
+
+/**
+ * Created by X1 on 2018/3/26.
+ */
+public class Moon {
+    private static Moon ourInstance = new Moon();
+
+    public static Moon getInstance() {
+        return ourInstance;
+    }
+
+    private Moon() {
+    }
+}
