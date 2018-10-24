64cb278e3d9837a30f1d3cc063db7fb42105b5bd
XiHaoTian <835584505@qq.com>
2018-10-24 20:31:00 +0800

    Problem102

--- src/Xi/LeetCode/OneHundredFifty/Problem102.java
+++ src/Xi/LeetCode/OneHundredFifty/Problem102.java
@@ -2,7 +2,10 @@ package Xi.LeetCode.OneHundredFifty;
 
 import Xi.LeetCode.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
@@ -10,6 +13,52 @@ import java.util.List;
 public class Problem102 {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+        List result = new ArrayList();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int currentLevelSize = 1;
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int temp = 0;
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode node = queue.remove();
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                    temp++;
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                    temp++;
+                }
+            }
+            currentLevelSize = temp;
+            result.add(levelList);
+        }
+        return result;
+    }
+
+    /**
+     * 层序遍历到一个list中
+     */
+    public List<Integer> levelOrderToOneList(TreeNode root) {
+        List result = new ArrayList();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (queue.peek() != null) {
+            TreeNode node = queue.remove();
+            result.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result;
     }
 }
