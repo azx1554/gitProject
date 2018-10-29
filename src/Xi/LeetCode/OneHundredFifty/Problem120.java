b51c220774a1ecffe6668d356ae9e795ed314c0b
XiHaoTian <835584505@qq.com>
2018-10-29 21:23:18 +0800

    Problem120

---
+++ src/Xi/LeetCode/OneHundredFifty/Problem120.java
@@ -0,0 +1,24 @@
+package Xi.LeetCode.OneHundredFifty;
+
+import java.util.List;
+
+/**
+ * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+ * <p>
+ * For example, given the following triangle
+ * <p>
+ * [ <br>
+ *    [2], <br>
+ *   [3,4], <br>
+ *  [6,5,7], <br>
+ * [4,1,8,3] <br>
+ * ] <br>
+ * The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+ */
+public class Problem120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        return 0;
+    }
+}
