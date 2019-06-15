81fa2ab1168e8f16f261b8e605e6ddcb31a744d3
XiHaoTian <835584505@qq.com>
2019-06-15 14:27:28 +0800

    problem 120 to 122

--- src/Xi/LeetCode/OneHundredFifty/Problem120.java
+++ src/Xi/LeetCode/OneHundredFifty/Problem120.java
@@ -17,8 +17,28 @@ import java.util.List;
  */
 public class Problem120 {
 
+    /**
+     * DP(bottom-up)
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
-
-        return 0;
+        int n = triangle.size();
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return triangle.get(0).get(0);
+        }
+        //result[k][i]代表第k行第i处的从下至上的最短路径
+        int[][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            result[n-1][i] = triangle.get(n-1).get(i);
+        }
+        for (int k = n-2; k >=0; k--) {
+            List<Integer> tempList = triangle.get(k);
+            for (int i = 0; i < tempList.size(); i++) {
+                result[k][i] = Math.min(result[k+1][i], result[k+1][i+1]) + tempList.get(i);
+            }
+        }
+        return result[0][0];
     }
 }
