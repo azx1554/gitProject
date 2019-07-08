0d91a709c5d2597568618974158dfcbfc2737133
XiHaoTian <835584505@qq.com>
2019-07-08 17:45:11 +0800

    Problem 214

---
+++ src/Xi/LeetCode/TwoHundredFifty/Problem214.java
@@ -0,0 +1,22 @@
+package Xi.LeetCode.TwoHundredFifty;
+
+/**
+ Given a string s, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+
+ Example 1:
+ Input: "aacecaaa"
+ Output: "aaacecaaa"
+
+ Example 2:
+ Input: "abcd"
+ Output: "dcbabcd"
+ */
+public class Problem214 {
+
+    /**
+     放着以后研究，KMP匹配算法的运用
+     */
+    public String shortestPalindrome(String s) {
+        return null;
+    }
+}
